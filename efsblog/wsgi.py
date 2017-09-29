852ca61c3e37a9d99d157f45a5091d90c1f8689f
luoshuitaotao <jcui@unomaha.edu>
2017-09-28 22:57:58 -0500

    change

--- efsblog/wsgi.py
+++ efsblog/wsgi.py
@@ -1,12 +1,16 @@
+"""
+WSGI config for efsblog project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
 import os
-import sys
-
-path ='/home/jingcui/efsblog'
-if path not in sys.path:
-    sys.path.append(path)
-
-os.environ['DJANGO_SETTING_MODULE'] = 'efsblog.settings'
 
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "efsblog.settings")
+
+application = get_wsgi_application()
